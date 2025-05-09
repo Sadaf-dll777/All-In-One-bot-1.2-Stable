f525226e8a13ed93e4b4ebd25ed0f35ddb214044
Sadaf-dll777 <md.saifalsadaf@gmail.com>
2025-05-09 23:12:05 +0600

    Update en.js

--- languages/en.js
+++ languages/en.js
@@ -66,7 +66,7 @@ const enLang = {
     replitLink: "https://replit.com/@mdsaifalsadaf",
     youtubeLink: "https://www.youtube.com/@clorexoffiicial",
     supportIconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&',
-    supportImageURL: 'https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&',
+    supportImageURL: 'https://cdn.discordapp.com/attachments/1293865045272563763/1370448135960137859/standard_8.gif?ex=681f88c9&is=681e3749&hm=4681bda1651d02c79ffe018efd4c6654910001e9bcc702baa4a1856059b4db7f&',
     supportDescriptionTitle: "Join our Discord server for support and updates",
     followUsOn: "Follow us on",
     discord: "Discord",
